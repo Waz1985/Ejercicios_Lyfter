c123d5c3a44524eca93f942b3c745e275c5070d4
Waz1985 <79213850+Waz1985@users.noreply.github.com>
2026-03-02 10:48:37 -0600

    Proyecto
    Proyecto

---
+++ Python/Proyecto_Gestor_finanzas_Personales/ui/movement_window.py
@@ -0,0 +1,25 @@
+from datetime import date
+import FreeSimpleGUI as sg
+
+
+
+def create_movement_window(categories, movement_type):
+    today = date.today().isoformat() 
+
+    layout = [
+        [sg.Text(f"Add {movement_type}")],
+        [sg.Text("Date (YYYY-MM-DD)"), sg.Input(default_text=today, key="-DATE-", size=(12, 1)), 
+            sg.CalendarButton("📅", target="-DATE-", format="%Y-%m-%d", close_when_date_chosen=True, no_titlebar=False)],
+        [sg.Text("Title"), sg.Input(key="-TITLE-")],
+        [sg.Text("Amount"), sg.Input(key="-AMOUNT-")],
+        [sg.Text("Category"), sg.Combo(categories, key="-CATEGORY-", readonly=True)],
+        [sg.Button("Save"), sg.Button("Cancel")]
+    ]
+
+    return sg.Window(
+        f"Add {movement_type}",
+        layout,
+        modal=True,
+        finalize=True
+    )
+
