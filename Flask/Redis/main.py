d4a97b18ffe70f76ab4a841ee0365cc909658cc3
Waz1985 <79213850+Waz1985@users.noreply.github.com>
2026-04-13 09:11:20 -0600

    redis y cache

---
+++ Flask/Redis/main.py
@@ -0,0 +1,7 @@
+from cache import CacheManager
+
+cache_manager = CacheManager(
+    host = "redis-12739.c84.us-east-1-2.ec2.cloud.redislabs.com",
+    port = 12739,
+    password = "MxIRGWpSSga8Pw3QooyHx5m7AiyMYUbl"    
+)
